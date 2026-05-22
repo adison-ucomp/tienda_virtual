29986c09ee997d2389da16311e4e4a1d520c6295
Adison Jimenez <adisonzenemij@gmail.com>
2026-05-22 00:47:43 -0500

    feat: 5fcefc18-a91f-4f05-952e-a8c03d3003da

--- app/src/main/java/com/compensar/tienda/model/EpaycoModel.kt
+++
@@ -1,8 +0,0 @@
-package com.compensar.tienda.model
-
-data class EpaycoModel(
-    val register: Long = 0,
-    val api: String? = null,
-    val state: String? = null,
-    val idOrder: Long = 0
-)
