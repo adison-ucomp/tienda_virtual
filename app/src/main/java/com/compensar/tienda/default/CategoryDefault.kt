b655c146eec825f64cf61e1e26363c4428b3462f
Adison Jimenez <adisonzenemij@gmail.com>
2026-05-21 22:05:23 -0500

    feat: de7441a4-734b-4b8e-a13f-30b1eecf56fe

--- app/src/main/java/com/compensar/tienda/default/CategoryDefault.kt
+++ app/src/main/java/com/compensar/tienda/default/CategoryDefault.kt
@@ -6,8 +6,16 @@ object CategoryDefault {
 
     fun getAll(): List<CategoryModel> {
         return listOf(
-            CategoryModel(register = 1, name = "Tecnologia"),
-            CategoryModel(register = 2, name = "Deportes")
+            CategoryModel(
+                register = 1,
+                name = "Tecnologia",
+                storefire = "https://firebasestorage.googleapis.com/v0/b/engineering-code.firebasestorage.app/o/images%2Fcategory%2F1_1779418996180.jpg?alt=media&token=b788cecb-eb34-4435-8979-a2f3273cc5c3"
+            ),
+            CategoryModel(
+                register = 2,
+                name = "Deportes",
+                storefire = "https://firebasestorage.googleapis.com/v0/b/engineering-code.firebasestorage.app/o/images%2Fcategory%2F2_1779419016167.jpg?alt=media&token=3fca3dd4-8bc7-47a0-87d1-96a5abc8720f"
+            )
         )
     }
 }
