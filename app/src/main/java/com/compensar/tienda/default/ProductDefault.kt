e29d1e7266fbfce8b91c056f0a14c8fdced56968
Adison Jimenez <adisonzenemij@gmail.com>
2026-05-21 20:13:08 -0500

    feat: c848435c-f2df-4c4d-b0ae-e65f55402a8c

--- app/src/main/java/com/compensar/tienda/default/ProductDefault.kt
+++ app/src/main/java/com/compensar/tienda/default/ProductDefault.kt
@@ -13,7 +13,7 @@ object ProductDefault {
                 stock = 10,
                 reserved = 0,
                 price = 100.0,
-                storefire = "Url",
+                storefire = "https://firebasestorage.googleapis.com/v0/b/engineering-code.firebasestorage.app/o/images%2Fproduct%2F1_1779213640610.jpg?alt=media&token=70a8296a-8d27-4eb9-a139-19644d411509",
                 idCategory = 1,
                 idShop = 1
             ),
@@ -24,7 +24,7 @@ object ProductDefault {
                 stock = 20,
                 reserved = 0,
                 price = 200.0,
-                storefire = "Url",
+                storefire = "https://firebasestorage.googleapis.com/v0/b/engineering-code.firebasestorage.app/o/images%2Fproduct%2F2_1779213669660.jpg?alt=media&token=71618d69-5337-4d6d-ba51-1dda042aba56",
                 idCategory = 2,
                 idShop = 2
             ),
@@ -35,7 +35,7 @@ object ProductDefault {
                 stock = 30,
                 reserved = 0,
                 price = 300.0,
-                storefire = "Url",
+                storefire = "https://firebasestorage.googleapis.com/v0/b/engineering-code.firebasestorage.app/o/images%2Fproduct%2F3_1779241702322.jpg?alt=media&token=f27c954e-d9e0-43ac-8cdd-8b5cc373c122",
                 idCategory = 1,
                 idShop = 3
             )
